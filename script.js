d1a3bcfb9cfd4275ebaaec7ccdd24294309412a9
Dimitris Theodosopoulos <155730305+dimtheo6@users.noreply.github.com>
2024-07-25 21:19:39 +0300

    added class for the constructor

--- script.js
+++ script.js
@@ -5,26 +5,24 @@ const books = document.querySelector('.books');
 
 let bookForm = document.getElementById('bookForm')
 
-// array to store the books
 const library = [];
 
-// define the Book constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+// Book class
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    static addBookToLibrary(title, author, pages, read) {
+        let newBook = new Book(title, author, pages, read);
+        library.push(newBook)
+        createCard(newBook);
+    }
 }
 
-//  function to add the book to the library
-function addBookToLibrary() {
-    let newBook = new Book(title.value, author.value, pages.value, read.checked);
-
-    library.push(newBook)
-
-    createCard(newBook);
-
-}
 
 // function to create the Book Card
 function createCard(book) {
@@ -35,13 +33,11 @@ function createCard(book) {
     const p2 = document.createElement('p');
     const p3 = document.createElement('p');
 
-    
     const b1 = document.createElement('button');
     const b2 = document.createElement('button');
 
     div.classList.add('book');
     div2.classList.add('group_button')
-    
 
     p1.classList.add('book_title');
     p2.classList.add('book_author');
@@ -57,27 +53,26 @@ function createCard(book) {
     b1.textContent = 'Have you read this?';
     b2.textContent = 'Delete'
 
-    // Append elements to the DOM
     books.appendChild(div);
-        div.appendChild(p1);
-        div.appendChild(p2);
-        div.appendChild(p3);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
     div.appendChild(div2);
-        div2.appendChild(b1);
-        div2.appendChild(b2);
+    div2.appendChild(b1);
+    div2.appendChild(b2);
 
     b1.textContent = book.read ? 'Read' : 'Not Read';
     b1.style.background = book.read ? 'rgba(1, 235, 1, 0.5)' : 'rgba(252, 37, 37, 0.5)';
-    
+
     //  Read Toggle Button
     b1.addEventListener('click', function () {
-        
+
         // Read - Not Read Toggle
         book.read = !book.read;
 
         b1.textContent = book.read ? 'Read' : 'Not Read';
         b1.style.background = book.read ? 'rgba(1, 235, 1, 0.5)' : 'rgba(252, 37, 37, 0.5)';
-        
+
     })
 
     // Store the book object as a property of the div
@@ -85,9 +80,7 @@ function createCard(book) {
 
     //      remove parent
     b2.addEventListener('click', function () {
-        // remove the parent
         div.remove();
-
         // remove the object from the array
         const index = library.indexOf(div.book);
 
@@ -96,28 +89,26 @@ function createCard(book) {
         }
     });
 
-}
 
+}
 
 //    Submit button
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let title = document.getElementById('title');
-    let author = document.getElementById('author');
-    let pages = document.getElementById('pages');
-    let read = document.getElementById('read');
-    1
-    addBookToLibrary();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
 
-    document.querySelector('form').reset();
+    Book.addBookToLibrary(title, author, pages, read);
+
+    bookForm.reset();
     dialog.close();
 
 })
 
-
 //  modal
-
 addButton.addEventListener('click', () => {
     dialog.showModal();
 })
